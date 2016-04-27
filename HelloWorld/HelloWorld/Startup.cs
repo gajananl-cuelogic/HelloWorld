1e6695609209aada146a4252b7a803bdb98528e4
gajananl-cuelogic <gajanan.londhe@cuelogic.co.in>
2016-04-27 15:34:21 +0530

    success

--- HelloWorld/HelloWorld/Startup.cs
+++ HelloWorld/HelloWorld/Startup.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
-        {  gjhjhgjgh
+        {  
             //updated on 4/27/2026 3.28
             ConfigureAuth(app);
         }
